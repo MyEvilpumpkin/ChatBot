6d6b225f026b60a4e00c49ea1d06f18abb07b2b3
MyEvilpumpkin <dima.tomin2010@yandex.ru>
2021-05-24 03:53:28 +0300

    Delete console logging

--- static/sw.js
+++ static/sw.js
@@ -8,10 +8,8 @@ const FILES_TO_CACHE = [
 ];
 
 self.addEventListener('install', (event) => {
-  console.log('[ServiceWorker] Install');
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      console.log('[ServiceWorker] Pre-caching offline page');
       return cache.addAll(FILES_TO_CACHE);
     })
   );
@@ -19,14 +17,11 @@ self.addEventListener('install', (event) => {
 });
 
 self.addEventListener('activate', (event) => {
-  console.log('[ServiceWorker] Activate');
   event.waitUntil(
     caches.keys().then((keyList) => {
       return Promise.all(keyList.map((key) => {
-        if (key !== CACHE_NAME) {
-          console.log('[ServiceWorker] Removing old cache', key);
+        if (key !== CACHE_NAME)
           return caches.delete(key);
-        }
       }));
     })
   );
